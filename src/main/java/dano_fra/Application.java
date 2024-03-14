7f127748512f88050330be1fe3884ea1ccb17c10
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-03-14 14:42:13 +0100

    Initial commit

---
+++ src/main/java/dano_fra/Application.java
@@ -0,0 +1,8 @@
+package dano_fra;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/dano_fra/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dano_fra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
